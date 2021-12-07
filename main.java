c993e73993d2eceadea56ae2ea7980df0bd51670
AriZox <arizoxfr@gmail.com>
2021-12-07 22:01:06 +0100

    creation et initialisation du plateau

--- main.java
+++ main.java
@@ -1,5 +1,29 @@
 class Codia extends Program {
+    void println(char[][] tab){
+        String result = "";
+        for (int i = 0; i<length(tab, 1); i++){
+            for (int j = 0; j<length(tab, 2); j++){
+                result += tab[i][j] + " ";
+            }
+            result += "\n";
+        }
+        println(result);
+    }
+
+    char[][] initPlateau(){
+        char[][] plateau = new char[][]{
+            {'#', '#', '#', '#', '#', '#', '#'},
+            {'#', '@', ' ', ' ', '/', '/', '#'},
+            {'#', '/', '/', ' ', '/', '/', '#'},
+            {'#', '/', '/', ' ', '/', '/', '#'},
+            {'#', '/', '/', '!', '/', '/', '#'},
+            {'#', '#', '#', '#', '#', '#', '#'}
+        };
+        return plateau;
+    }
+
     void algorithm(){
-        println("Hello World");
+        char[][] plateau = initPlateau();
+        println(plateau);
     }
 }
