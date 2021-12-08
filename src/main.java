55d9473c717fcf652a7a93289c052b505bc2dd33
AriZox <arizoxfr@gmail.com>
2021-12-08 09:28:56 +0100

    added instructions and choix with print

--- src/main.java
+++ src/main.java
@@ -10,15 +10,16 @@ class Codia extends Program {
         println(result);
     }
 
-    char[][] initPlateau(){
+    char[][] initPlateau(Joueur joueur){
         char[][] plateau = new char[][]{
             {'#', '#', '#', '#', '#', '#', '#'},
-            {'#', '@', ' ', ' ', '/', '/', '#'},
+            {'#', ' ', ' ', ' ', '/', '/', '#'},
             {'#', '/', '/', ' ', '/', '/', '#'},
             {'#', '/', '/', ' ', '/', '/', '#'},
             {'#', '/', '/', '!', '/', '/', '#'},
             {'#', '#', '#', '#', '#', '#', '#'}
         };
+        plateau[joueur.pos.i][joueur.pos.y] = joueur.car;
         return plateau;
     }
 
@@ -30,6 +31,25 @@ class Codia extends Program {
         joueur.pos.i = i;
         joueur.pos.y = y;
         return joueur;
+    }
+
+    void afficherInstructions(String[] instructions){
+        String result = "Instructions : {";
+        for (int i = 0; i<length(instructions); i++){
+            if (instructions[i] != null){
+                result += instructions[i] + ";";
+            }
+        }
+        result += "}";
+        println(result);
+    }
+
+    void afficherChoix(String[] choix){
+        String result = "Choisir une instruction : \n\n";
+        for (int i = 0; i<length(choix); i++){
+            result += "  [" + (i+1) + "] - " + choix[i] + "\n";
+        }
+        println(result);
     }
 
     void haut(char[][] plateau, Joueur joueur){
@@ -50,7 +70,36 @@ class Codia extends Program {
 
     void algorithm(){
         Joueur joueur = initJoueur(1, 1);
-        char[][] plateau = initPlateau();
+        char[][] plateau = initPlateau(joueur);
         println(plateau);
+        String[] instructions = new String[20];
+
+        String[] choix = new String[]{"haut", "bas", "droite", "gauche", "fin"};
+        int indexChoix = 0;
+        int entreeInt = 0;
+        do {
+            afficherInstructions(instructions);
+            afficherChoix(choix);
+            print("Votre choix : ");
+            entreeInt = readInt();
+            instructions[indexChoix] = choix[entreeInt-1];
+            indexChoix++;
+        } while (!equals(choix[entreeInt-1], "fin"));
+
+        int indexInstructions = 0;
+        // while (instructions[index] != null){
+        //     String instruction = instructions[i];
+        //     switch (instruction){
+        //         case "haut":
+        //             haut(plateau, joueur);
+        //         case "bas":
+        //             bas(plateau, joueur);
+        //         case "droite":
+        //             droite(plateau, joueur);
+        //         case "gauche":
+        //             gauche(plateau, joueur);
+        //     }
+        //     index++;
+        // }
     }
 }
